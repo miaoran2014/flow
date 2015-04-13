5dcb073b4ef22d8520eb167e1290a7941778561c
miaoran <1822773181@qq.com>
2015-04-13 15:49:35 +0800

    3:自定义布局基本实现

--- 瀑布流/瀑布流/MRLayout.m
+++ 瀑布流/瀑布流/MRLayout.m
@@ -15,18 +15,34 @@ static CGFloat const margin=10;
 - (void)prepareLayout{
     [super prepareLayout];
 }
+
+//一次性获取所有cell,补充,装饰的布局属性
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect{
     NSInteger count=[self.collectionView numberOfItemsInSection:0];
-    NSLog(@"%zu",count);
     NSMutableArray* arrM=[NSMutableArray array];
     for (int i=0; i<count; i++) {
         NSIndexPath* path=[NSIndexPath indexPathForItem:i inSection:0];
         UICollectionViewLayoutAttributes* attr=[self layoutAttributesForItemAtIndexPath:path];
+        [arrM addObject:attr];
     }
-    return nil;
+    return arrM;
 }
+//为layoutAttributesForElementsInRect返回的数组里的布局属性进行布局
 - (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath{
-    return nil;
+    //获取self layoutAttributesForItemAtIndexPath:path 传递过来 指定item的布局属性
+    UICollectionViewLayoutAttributes* attr=[UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+    /**
+     * @property (nonatomic) CGRect frame;
+     @property (nonatomic) CGPoint center;
+     @property (nonatomic) CGSize size;
+     @property (nonatomic) CATransform3D transform3D;
+     @property (nonatomic) CGRect bounds NS_AVAILABLE_IOS(7_0);
+     @property (nonatomic) CGAffineTransform transform NS_AVAILABLE_IOS(7_0);
+     @property (nonatomic) CGFloat alpha;
+     @property (nonatomic) NSInteger zIndex;
+     */
+    attr.frame=CGRectMake(100, 100, 100, 100);
+    return attr;
 }
 
 @end
